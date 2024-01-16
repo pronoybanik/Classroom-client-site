ec1707868fc30cba03edb2398a8db8285a754b12
PRONOY <pronoybanik82@gmail.com>
2024-01-16 15:53:22 +0600

    change interface

--- src/component/HomeCard/HomeCard.jsx
+++ src/component/HomeCard/HomeCard.jsx
@@ -25,57 +25,60 @@ const HomeCard = ({ handleDelete, classInfo }) => {
   }, [teacherId]);
 
   return (
-    <Link
-      to={`${
-        classRole === "teacher" ? `/classId/${_id}` : `/classId/${teacherId}`
-      }`}
-    >
-      <div className="max-w-md mt-4 p-4 shadow-md rounded-2xl space-y-8 hover:bg-slate-100">
-        <div className="relative">
-          <img
-            className="w-full h-full rounded-2xl bg-gray-500"
-            src={imgUrl}
-            alt=""
-          />
-          <img
-            className="w-[100px] h-[100px] absolute -bottom-10 left-1/2 -translate-x-1/2 rounded-full bg-gray-400 border border-white"
-            src={teacherImage ? teacherImage : teacherData?.teacherImage}
-            alt=""
-          />
-        </div>
-        {/* profile name & role */}
-        <div className="text-center space-y-1 ">
-          <h1 className="text-2xl pt-2 font-semibold">
-            {teacherName ? teacherName : teacherData?.teacherName}
-          </h1>
-          <p className="text-gray-500 text-sm font-bold uppercase pt-5">
-            Role: {classRole}
-          </p>
-        </div>
-        <div>
-          <h3 className="mt-4  text-lg font-bold text-gray-900">{className}</h3>
-
-          <p className="mt-1.5  text-sm font-semibold text-gray-700">
-            section: {section}
-          </p>
-          {subject ? (
-            <p className="mt-1.5  text-sm font-semibold text-gray-700">
-              section: {subject}
+    <div className="max-w-md mt-4 p-4 shadow-md rounded-2xl space-y-8 hover:bg-slate-100">
+      <div>
+        <Link
+          to={`${
+            classRole === "teacher"
+              ? `/classId/${_id}`
+              : `/classId/${teacherId}`
+          }`}
+        >
+          <div className="relative">
+            <img
+              className="w-full h-full rounded-2xl bg-gray-500"
+              src={imgUrl}
+              alt=""
+            />
+            <img
+              className="w-[100px] h-[100px] absolute -bottom-10 left-1/2 -translate-x-1/2 rounded-full bg-gray-400 border border-white"
+              src={teacherImage ? teacherImage : teacherData?.teacherImage}
+              alt=""
+            />
+          </div>
+          {/* profile name & role */}
+          <div className="text-center space-y-1 pt-8">
+            <h1 className="text-2xl pt-2 font-semibold">
+              {teacherName ? teacherName : teacherData?.teacherName}
+            </h1>
+            <p className="text-gray-500 text-sm font-bold uppercase pt-5">
+              Role: {classRole}
             </p>
-          ) : null}
+          </div>
+          <div>
+            <h3 className="mt-4  text-lg font-bold text-gray-900">
+              {className}
+            </h3>
 
-          {/* {classRole === "teacher" ? ( */}
-          <form className="mt-4">
-            <button
-              onClick={() => handleDelete(_id)}
-              className="block w-full rounded bg-black text-white p-2 text-sm font-medium transition hover:scale-105"
-            >
-              leave Class
-            </button>
-          </form>
-        </div>
+            <p className="mt-1.5  text-sm font-semibold text-gray-700">
+              section: {section}
+            </p>
+
+            <p className="mt-1.5  text-sm font-semibold text-gray-700">
+              subject: {subject}
+            </p>
+          </div>
+        </Link>
       </div>
-    </Link>
+      <div className="mt-4">
+        <button
+          onClick={() => handleDelete(_id)}
+          className="block w-full rounded bg-black text-white p-2 text-sm font-medium transition hover:scale-105"
+        >
+          leave Class
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/component/SideMenuComponents/SideMenuComponents.jsx
+++ src/component/SideMenuComponents/SideMenuComponents.jsx
@@ -49,13 +49,13 @@ const SideMenuComponents = () => {
                 </Link>
 
                 <Link
-                  to="/home"
+                  to="/calendar"
                   className="mt-4 group relative flex justify-center rounded bg-blue-50 px-2 py-1.5 text-blue-700"
                 >
-                  <SlCalender/>
+                  <SlCalender />
 
                   <span className="absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100">
-                    home
+                    calendar
                   </span>
                 </Link>
               </div>
