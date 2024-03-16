1b098278d67580637f57bd78213916cf05a5d3a4
PRONOY <pronoybanik82@gmail.com>
2024-03-16 09:43:52 +0600

    change meet id

--- src/component/RoomSection/RoomSection.jsx
+++ src/component/RoomSection/RoomSection.jsx
@@ -13,9 +13,11 @@ const RoomSection = () => {
 
   const myMeeting = async (element) => {
     // const appId = 1464188188;
-    const appId = 1134648179;
+    // const appId = 1134648179;
+    const appId = 1907519381;
     // const serverSecret = "aa6cf07b7e98d46e0a85ca8a9237c179";
-    const serverSecret = "518430d9c74b17071292ecef41ade196";
+    // const serverSecret = "518430d9c74b17071292ecef41ade196";
+    const serverSecret = "a707ebd5abca831d6fb718d162c293f4";
 
     const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
       appId,
