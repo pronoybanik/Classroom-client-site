5e4b2e052d80170fcfaddc998982d8b2aba6ce78
PRONOY <pronoybanik82@gmail.com>
2024-11-06 20:09:32 +0600

    change metsystem code

--- src/component/RoomSection/RoomSection.jsx
+++ src/component/RoomSection/RoomSection.jsx
@@ -15,11 +15,13 @@ const RoomSection = () => {
     // const appId = 1464188188;
     // const appId = 1134648179;
     // const appId = 1907519381;
-    const appId = 1337293372;
+    // const appId = 1337293372;
+    const appId = 576961043;
     // const serverSecret = "aa6cf07b7e98d46e0a85ca8a9237c179";
     // const serverSecret = "518430d9c74b17071292ecef41ade196";
     // const serverSecret = "a707ebd5abca831d6fb718d162c293f4";
-    const serverSecret = "9cfe819bc048b342321937040466bff0";
+    // const serverSecret = "9cfe819bc048b342321937040466bff0";
+    const serverSecret = "c5b21bf9545d7553c7130e3444424e98";
 
     const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
       appId,
